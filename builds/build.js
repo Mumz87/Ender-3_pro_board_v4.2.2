ae1c723f5bd5a3e4f935ab37bbc9124ec9996f05
Mumz87 <mumz1987@gmail.com>
2024-01-04 18:07:17 +0700

    Update build.js

--- builds/build.js
+++ builds/build.js
@@ -1,30 +1,21 @@
 module.exports = {
-    board_env: "STM32F103RE_creality",
-    active: true,
-    min_version: "2.1.2",
+    extends: "builds/default.js",
     meta: {
-        stable_name: "ender_3_4.2.2-{{marlin_version}}-{{uid}}",
-        nightly_name: "ender_3_4.2.2-{{current_date}}-{{uid}}"
-    },
-    based_on: {
-        repo: "https://github.com/MarlinFirmware/Configurations/",
-        path: "/config/examples/Creality/Ender-3/CrealityV422/",
-        stable_branch: "release-{{marlin_version}}",
-        nightly_branch: "bugfix-2.1.x"
+        stable_name: "ender_3_4.2.2-{{marlin_version}}-base-{{uid}}",
+        nightly_name: "ender_3_4.2.2-{{current_date}}-base-{{uid}}"
     },
     configuration: {
         enable: [
+            ["STRING_CONFIG_H_AUTHOR", "(Dust, valerionew, Ender-3, zisismaras)"],
             //standard leveling menu helper
             "LCD_BED_TRAMMING",
             "BED_TRAMMING_INCLUDE_CENTER"
-        ],
-        disable: []
+        ]
     },
     configuration_adv: {
         enable: [
             //octoprint
             "HOST_ACTION_COMMANDS"
-        ],
-        disable: []
+        ]
     }
 };
